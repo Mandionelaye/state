b3a038a56e3b63a057f743ed001891c51ab6b129
buurjoie <seydinamandionem9@gmail.com>
2023-03-18 22:59:35 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect,useRef} from 'react';
 import Profile from './profill/profile';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-export default class App extends Component {
-   constructor(props){
-     super(props);
-     this.state={person:{
-      FullName:"Seydina Mandione mbaye", 
-      Bio:"Developpeur Full Stack et aussi etudiant en informatique de developpement d'application web mobile a l'Universite Virtuelle du Senegal et a Gomycode. J'ai 24ans et je suis en 2e annee de licence.", 
-      imgSrc: '/img1.jpg', 
-      Profession:"Developpeur Full Stack",
-      show:false, }
-   }}
-   modif=()=>this.setState({show:!this.state.show});
- 
-  render(){
+export default function App(){
+   
+const [FullName,setFullName]= useState("Seydina Mandione mbaye");
+const [Bio,setBio]= useState("Developpeur Full Stack et aussi etudiant en informatique de developpement d'application web mobile a l'Universite Virtuelle du Senegal et a Gomycode. J'ai 24ans et je suis en 2e annee de licence.");
+const [imgSrc,setimgSrc]= useState('/img1.jpg');
+const [Profession,setProfession]= useState("Developpeur Full Stack");
+const [show,setShow]= useState(false);
+const modif=()=>setShow(!show);
+const showdefault = useRef(false);
+
+
   return (
     <>
     <div className="body">
     <div className="container">
-      <button className='btn btn-outline-primary m-3'onClick={this.modif} >bascule</button>
-         { this.state.show ? 
-              <Profile fullName={this.state.person.FullName} bio={this.state.person.Bio} profession={this.state.person.Profession} img={this.state.person.imgSrc}></Profile>
+      <button className='btn btn-outline-primary m-3'onClick={modif} >bascule</button>
+         { show ? 
+              <Profile fullName={FullName} bio={Bio} profession={Profession} img={imgSrc}></Profile>
           :null
          }
          </div>
@@ -32,4 +30,20 @@ export default class App extends Component {
 
   );
 }
-}
+
+// export default class App extends Component {
+//    constructor(props){
+//      super(props);
+//      this.state={person:{
+//       FullName:"Seydina Mandione mbaye", 
+//       Bio:"Developpeur Full Stack et aussi etudiant en informatique de developpement d'application web mobile a l'Universite Virtuelle du Senegal et a Gomycode. J'ai 24ans et je suis en 2e annee de licence.", 
+//       imgSrc: '/img1.jpg', 
+//       Profession:"Developpeur Full Stack",
+//       show:false, }
+//    }}
+//    modif=()=>this.setState({show:!this.state.show});
+ 
+//   render(){
+
+// }
+// }
--- src/profill/profile.js
+++ src/profill/profile.js
@@ -1,59 +1,75 @@
-import React, {Component} from 'react';
+import React, {useState,useEffect} from 'react';
 import propTypes from 'prop-types'; 
 import image2 from "./photo.jpeg";
-class Profile extends Component{
+
+
+ function Profile({fullName, bio, profession, img}) {
+  const [timer, setTimer]= useState(0);
+
+       useEffect(()=>{
+         const intervalID = setInterval(() => setTimer(timer + 1), 1000);
+         return ()=>clearInterval(intervalID);
+       });
+
     
-  constructor(props){
-      super(props);
-    this.state = {
-      timer: 0,
-    };
-  
-  }
+
+    const stylename = {color:"rgb(120, 48, 183)",fontWeight: "bolder",fontSize: "30px",};
+    const styleprfs={ color:'rgb(218, 255, 11)',fontWeight:'bolder'};
+    const divestyle={  paddingTop: "10px",paddingLeft:"5px"};
+    const block1 = {  display: "flex", flexDirection: "row",  paddingTop: "30px"};
+    const block2 = {  display: "flex",flexDirection: "column",justifyContent: "center"};
+    const img2 = { minWidth: "70px"};
+    const img1 = { width: "18vmin", borderRadius: "10px"};
+     const time = {color: "rgb(0, 140, 255)" };
+    return(
+      <>
+     <div className="row">
+        <div className="col col-md-12" style={block1}>
+          <img src={img} style={img1} alt="img" />
+        <div style={divestyle}>   
+        <h1 style={stylename}>{fullName}</h1>
+        <h3 style={styleprfs}>{profession}</h3>
+        <p style={time}> l'intervalle de temps depuis le montage du dernier composant est de : {timer}s</p>
+        </div>
+        </div>
+        <div className='col-md-12' style={block2}>
+        <h4>{bio}</h4>
+        <img src={image2} style={img2}  alt="img2" />
+        </div>
+    </div>
+    </>
+    );
+}
+
+// class Profile extends Component{
+    
+//   constructor(props){
+//       super(props);
+//     this.state = {
+//       timer: 0,
+//     };
+
+//   }
    
 
-  componentDidMount(){
-  this.intervalID = setInterval(() => this.tictac(), 1000);
-    }
-   componentWillUnmount(){
-     clearInterval(this.intervalID);
-   }
+//   componentDidMount(){
+//   this.intervalID = setInterval(() => this.tictac(), 1000);
+//     }
+//    componentWillUnmount(){
+//      clearInterval(this.intervalID);
+//    }
  
-  tictac(){
-   this.setState({
-    timer: this.state.timer + 1
-   })
-  }
+//   tictac(){
+//    this.setState({
+//     timer: this.state.timer + 1
+//    })
+//   }
 
-  render(){
-  const stylename = {color:"rgb(120, 48, 183)",fontWeight: "bolder",fontSize: "30px",};
-const styleprfs={ color:'rgb(218, 255, 11)',fontWeight:'bolder'};
-const divestyle={  paddingTop: "10px",paddingLeft:"5px"};
-const block1 = {  display: "flex", flexDirection: "row",  paddingTop: "30px"};
-const block2 = {  display: "flex",flexDirection: "column",justifyContent: "center"};
-const img2 = { minWidth: "70px"};
-const img1 = { width: "18vmin", borderRadius: "10px"};
-const time = {color: "rgb(0, 140, 255)" };
-  return(
-    <>
-   <div className="row">
-      <div className="col col-md-12" style={block1}>
-        <img src={this.props.img} style={img1} alt="img" />
-      <div style={divestyle}>   
-      <h1 style={stylename}>{this.props.fullName}</h1>
-      <h3 style={styleprfs}>{this.props.profession}</h3>
-      <p style={time}> l'intervalle de temps depuis le montage du dernier composant est de : {this.state.timer}s</p>
-      </div>
-      </div>
-      <div className='col-md-12' style={block2}>
-      <h4>{this.props.bio}</h4>
-      <img src={image2} style={img2}  alt="img2" />
-      </div>
-  </div>
-  </>
-  );
- };
-}
+//   render(){
+
+
+//  };
+// }
 
 Profile.propTypes={
     fullName:propTypes.string,
